a24845fff1fa3500ca761ad5dc80f94349ebf938
“zjwu0522” <zijian.wu@u.nus.edu>
2025-08-02 17:09:56 +0000

    chore: corrrect env name

--- src/services.py
+++ src/services.py
@@ -82,7 +82,7 @@ SERVICES = {
             # Evaluation organisation / user that hosts ephemeral test repositories
             "eval_org": {
                 "env_var": "GITHUB_EVAL_ORG",
-                "default": "MCPLeague-Eval",
+                "default": "mcpleague-eval",
                 "required": False,
                 "description": "Evaluation organisation or user for creating temporary test repositories"
             },
